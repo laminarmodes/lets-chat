5da308da6f3e1b77893d99f1196bf4882ac68a5b
laminarmodes <anya.traille@gmail.com>
2022-03-02 23:54:34 +0800

    Compose messages

--- components/Chat.js
+++ components/Chat.js
@@ -1,12 +1,62 @@
 import React from 'react';
-import { View, Text } from 'react-native';
-import { useEffect } from 'react';
-import { backgroundColor } from 'react-native/Libraries/Components/View/ReactNativeStyleAttributes';
+import { View, Text, Platform, KeyboardAvoidingView } from 'react-native';
+import { GiftedChat } from 'react-native-gifted-chat';
+import { back } from 'react-native/Libraries/Animated/Easing';
 
 
 export default class Chat extends React.Component {
-    render() {
 
+    constructor() {
+        super();
+        this.state = {
+            messages: []
+        }
+    }
+
+    componentDidMount() {
+        this.setState({
+            messages: [
+                {
+                    _id: 1,
+                    text: 'Hello developer',
+                    createdAt: new Date(),
+                    user: {
+                        _id: 1,
+                        name: 'React Native',
+                        avatar: 'https://placeimg.com/140/140/any'
+                    }
+                },
+                {
+                    _id: 2,
+                    text: 'How are you?',
+                    createdAt: new Date(),
+                    user: {
+                        _id: 2,
+                        name: 'Guest',
+                        avatar: 'https://placeimg.com/140/140/any'
+                    }
+                },
+                {
+                    _id: 3,
+                    text: 'Hi',
+                    createdAt: new Date(),
+                    user: {
+                        _id: 3,
+                        name: 'React Native',
+                        avatar: 'https://placeimg.com/140/140/any'
+                    }
+                }
+            ],
+        })
+    }
+
+    onSend(messages = []) {
+        this.setState(previousState => ({
+            messages: GiftedChat.append(previousState.messages, messages),
+        }))
+    }
+
+    render() {
         let name = this.props.route.params.name;
         let backgroundColor = this.props.route.params.backgroundColor;
 
@@ -15,15 +65,23 @@ export default class Chat extends React.Component {
         });
 
         return (
-            <View
-                style={{
-                    flex: 1,
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                    backgroundColor: backgroundColor
-                }}>
-                <Text>Hello {name} </Text>
-                {/* <Text>The color selected was {backgroundColor} </Text> */}
+
+            <View style={{
+                flex: 1,
+                backgroundColor: backgroundColor
+            }}>
+                <GiftedChat
+                    messages={this.state.messages}
+                    onSend={(messages) => this.onSend(messages)}
+                    user={{
+                        _id: 1,
+                    }}
+                />
+                {
+                    Platform.OS === "android" ? (
+                        <KeyboardAvoidingView behavior="height" />
+                    ) : null
+                }
             </View>
         );
     };
